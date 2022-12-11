996794c3678d7f01f4a04386dd8a3218c2d71536
Étienne Boucher <etienneboucher@hotmail.ca>
2022-12-10 21:37:41 -0500

    Add possibility to have log scale axis in figures

--- visualization/figure.py
+++ visualization/figure.py
@@ -1,8 +1,8 @@
 import plotly.express as px
 import pandas as pd
 
-def figure(df: pd.DataFrame, params: dict=None):
-    fig = px.line(df, x="x", y="y", facet_row="row", facet_col="column", color="Method")
+def figure(df: pd.DataFrame, log_x: bool=False, log_y: bool=False):
+    fig = px.line(df, x="x", y="y", facet_row="row", facet_col="column", color="Method", log_x=log_x, log_y=log_y)
     fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     
     return fig
