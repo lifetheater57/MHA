eb68b56d7fd0e3ecf7d0763032f0ba385c64c09a
Étienne Boucher <etienneboucher@hotmail.ca>
2022-12-12 14:06:19 -0500

    Add confidence interval plotting

--- visualization/figure.py
+++ visualization/figure.py
@@ -32,6 +32,7 @@ def figure(df: pd.DataFrame, config: dict):
                 color = colors[method]
                 x = df.loc[mask, get_at(config["columns"]["x"], j)]
                 y = df.loc[mask, get_at(config["columns"]["y"], j)]
+                y_confidence = df.loc[mask, get_at(config["columns"]["y_confidence"], j)]
                 fig.add_trace(
                     go.Scatter(
                         x=x, 
@@ -46,6 +47,21 @@ def figure(df: pd.DataFrame, config: dict):
                     col=j+1
                 )
                 
+                if y_confidence.notna().all() and (y_confidence > 0).all():
+                    fig.add_trace(
+                        go.Scatter(
+                            x=pd.concat([x, x[::-1]]), 
+                            y=pd.concat([y + y_confidence, (y - y_confidence)[::-1]]),
+                            fill="toself",
+                            fillcolor=f"rgba{color[3:-1]},0.2)",
+                            line=dict(color='rgba(255,255,255,0)'),
+                            hoverinfo="skip",
+                            showlegend=False,
+                        ),
+                        row=i+1, 
+                        col=j+1
+                    )
+
                 # Update axis properties
                 fig.update_xaxes(title_text=get_at(config["columns"]["x"], j), row=i+1, col=j+1)
                 fig.update_yaxes(title_text=get_at(config["columns"]["y"], j), row=i+1, col=j+1)
