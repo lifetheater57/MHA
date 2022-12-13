a63edd8be09f1a9a9729d72dfd8a935b16b9b9e7
Iggy1055 <119620783+Iggy1055@users.noreply.github.com>
2022-12-13 15:55:23 -0500

    Update Projet IFT6269 EM non-identifiable.py

--- model/Projet IFT6269 EM non-identifiable.py
+++ model/Projet IFT6269 EM non-identifiable.py
@@ -1,146 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[26]:
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-# In[27]:
-
-
-def ProjectNonNegative( W ):
-    """
-    projection onto the non-negative orthant
-    """
-
-    return W * ( W > 0 )
-
-
-def ProjectMax1( W ):
-    """
-    project onto set of non-neg matricies with one nonzero entry per row
-    """
-    for i in range( W.shape[0] ):
-        max_id = W[ i,: ].argmax()
-        W[i,:][ np.where( W[ i,: ] < W[ i,max_id ] )[0] ] = 0
-        W[i, max_id] = max(0, W[i, max_id])
-
-    return W 
-
-def normalizeColumns( W ):
-    """
-    standardize columns of W to be unit norm
-    """
-    for i in range( W.shape[1] ):
-        W[:,i] /= ( np.linalg.norm( W[:,i] ) + .001 )
-
-    return W
-
-
-# In[28]:
-
-
-from typing import Tuple, Union
-
-import numpy as np
-
-class GaussianGenerator(object):
-    """ Generator for high-dimensional gaussian data
-    
-    Attributes
-    ----------
-    W : array([p, k])
-        Factor loadings
-    G : array([N, k, k])
-        Covariance structures of latent variables
-    v : array([N])
-        Class-wise noise
-
-    Yields
-    ------
-    X : array([N, p])
-        Generated data (1 p-dimensional observation per class)
-
-    """
-
-    def __init__(self, N: int, p: int, k: int, seed: int, size: Union[int, Tuple[int]] = 1):
-        """
-        Parameters
-        ----------
-        N : int
-            Nb of classes, aka number of observation classes sharing
-            the same factor loadings
-        p : int
-            Dimensionality of the observations
-        k : int
-            Number of latent variables
-        seed : int
-            RNG seed
-        size : int|tuple(int)
-            The size of the samples generated for each class. The shape of the output is `N x size x p`.
-        """
-        # Fix seed for reproducibility
-        np.random.seed(seed)
-        
-        epsilon = 1e-10
-
-        self.N = N
-        self.p = p
-        self.k = k
-        self.size = size
-
-        # Create factor loading matrix W
-        # Constraints on F: orthonormal and non-negative
-        indexes = np.random.randint(0, k, size=p)
-        self.W = np.random.uniform(0, 1, size=(p, k))
-        #self.W = self.W * np.eye(k)[indexes]
-        #self.W /= np.linalg.norm(self.W, axis=0)
-        self.W = ProjectMax1(self.W)
-        self.W = normalizeColumns(self.W)
-        self.W = normalizeColumns(self.W)
-
-        # Create latent variable covariance G_i
-        # We use a triangular matrix to enforce a positive semi-definite covariance matrix
-        self.G = [np.tril(X) @ np.tril(X).T for X in np.random.normal(size=(N, k, k))]
-
-        self.v = np.random.normal(1, 1, size=N)**2
-        # self.v = np.ones(N)
-
-        # assert constraints on W and G
-        # assert (np.linalg.norm(self.W, axis=0) - np.ones(k) < epsilon).all(), \
-        #    "Columns of W are not unit vectors."
-        # assert (np.abs((self.W.T @ self.W) - np.eye(k)) < epsilon).all(), \
-        #    "W is not orthogonal."
-        # assert np.array([G_i == G_i.T for G_i in self.G]).all(), \
-        #    "Covariance matrices are not symmetrical."
-        
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return self.next()
-
-    def next(self):
-        Z = [np.random.multivariate_normal(np.zeros(G_i.shape[0]), G_i, size=self.size) for G_i in self.G]
-        X = np.array([[np.random.multivariate_normal(self.W @ z_i, v_i * np.eye(self.p)) for z_i in Z_i] for Z_i, v_i in zip(Z, self.v)])
-        return X
-
-
-# In[29]:
-
-
-gen = GaussianGenerator(10, 50, 5, 420, 500)
-X = gen.next()
-print(X.shape)
-
-
-# In[30]:
-
-
 class ConnectivityEM:
     
     def __init__(self, X, k):
@@ -167,7 +24,7 @@ class ConnectivityEM:
             
         self.G = np.array([self.W.T @ self.K[i] @ self.W - np.eye(self.k) for i in range(self.N)])
          
-        self.sigmas = np.concatenate([(self.W @ self.G[i] @ self.W.T                                       + self.v[i]*np.eye(self.p))[np.newaxis, ...] for i in range(self.N)])
+        self.sigmas = np.concatenate([(self.W @ self.G[i] @ self.W.T + self.v[i]*np.eye(self.p))[np.newaxis, ...] for i in range(self.N)])
         
         self.lagrange = np.zeros((self.k,self.k))
         
@@ -226,9 +83,9 @@ class ConnectivityEM:
             W_grad = np.zeros(self.W.shape)
             
             # E-step
-            self.mu_z = np.array([[(self.W @ self.G[i]).T @                              np.linalg.lstsq(self.sigmas[i], self.X[i][j], rcond=None)[0] for j in range(self.n)] for i in range(self.N)])
+            self.mu_z = np.array([[(self.W @ self.G[i]).T @ np.linalg.lstsq(self.sigmas[i], self.X[i][j], rcond=None)[0] for j in range(self.n)] for i in range(self.N)])
         
-            self.sigmas_z = np.array([self.G[i] - (self.W @ self.G[i]).T @                                   np.linalg.lstsq(self.sigmas[i], self.W @ self.G[i], rcond=None)[0] for i in range(self.N)])
+            self.sigmas_z = np.array([self.G[i] - (self.W @ self.G[i]).T @ np.linalg.lstsq(self.sigmas[i], self.W @ self.G[i], rcond=None)[0] for i in range(self.N)])
         
             self.E_z = self.mu_z
             
@@ -239,7 +96,7 @@ class ConnectivityEM:
                 for j in range(self.n):
                     self.G[i] += self.E_z_2[i][j]
                     
-                    self.v[i] += (0.5*np.sum(self.X[i][j]**2) - self.E_z[i][j] @ self.W.T @ self.X[i][j]) +                                  0.5*np.trace(self.E_z_2[i][j] @ self.W.T @ self.W)
+                    self.v[i] += (0.5*np.sum(self.X[i][j]**2) - self.E_z[i][j] @ self.W.T @ self.X[i][j]) + 0.5*np.trace(self.E_z_2[i][j] @ self.W.T @ self.W)
                                    
                 self.G[i] /= self.n
                                    
@@ -258,7 +115,7 @@ class ConnectivityEM:
                                    
             self.W = term_1_W @ np.linalg.pinv(term_2_W)
             
-            self.sigmas = np.concatenate([(self.W @ self.G[i] @ self.W.T                                       + self.v[i]*np.eye(self.p))[np.newaxis, ...] for i in range(self.N)])
+            self.sigmas = np.concatenate([(self.W @ self.G[i] @ self.W.T + self.v[i]*np.eye(self.p))[np.newaxis, ...] for i in range(self.N)])
         
             old_likelihood = new_likelihood
             new_likelihood = self.negative_log_likelihood()
@@ -271,32 +128,3 @@ class ConnectivityEM:
         print("G:\n", self.G)
         print("W\n", self.W)
         print("sigmas\n", self.sigmas)
-
-
-# In[31]:
-
-
-k = 5
-connect = ConnectivityEM(X, k)
-print(connect.W.shape)
-print(connect.G.shape)
-print(connect.sigmas.shape)
-
-# LL = connect.negative_log_likelihood()
-# print(LL)
-
-#for i in range(connect.N):
-#    d, V = np.linalg.eig(connect.G[i])
-#    v_i = connect.v[i]
-#    A = V @ np.diag(d/(d + v_i)) @ V.T
-#
-#    D_tilde = V @ np.diag(-d/(d+v_i)**2) @ V.T
-#
-#    D_bar = V.T @ np.diag(-2*d**2/(d+v_i)**3) @ V.T
-#    print("d ", d)
-#    print("V ", V)
-LLs = connect.fit()
-# print(len(LLs))
-#plt.xscale('log')
-plt.plot(LLs)
-
