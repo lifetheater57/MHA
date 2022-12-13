9a24cbc8ab7ec0b8609ae94541e61d3add635e8e
Iggy1055 <119620783+Iggy1055@users.noreply.github.com>
2022-12-13 00:22:15 -0500

    Create Helper_Functions.py

---
+++ model/Helper_Functions.py
@@ -0,0 +1,14 @@
+def relu(X):
+    X[X < 0] = 0
+    return X
+
+def project_positive(X):
+    max_idx = X.argmax(axis=1)
+    for i in range(X.shape[0]):
+        X[i, X[i,:] < X[i, max_idx[i]]] = 0
+        X = relu(X)
+    return X
+
+def normalize_cols(X, eps=0.001):
+    X = X / (np.linalg.norm(X, axis=0) + eps)
+    return X
