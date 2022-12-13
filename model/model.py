9355e67a7e805e4b93a00a84bfb5197f875f267b
Iggy1055 <119620783+Iggy1055@users.noreply.github.com>
2022-12-13 00:19:41 -0500

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,27 +1,10 @@
-import numpy as np
-# Helper functions
-def relu(X):
-    X[X < 0] = 0
-    return X
-
-def project_positive(X):
-    max_idx = X.argmax(axis=1)
-    for i in range(X.shape[0]):
-        X[i, X[i,:] < X[i, max_idx[i]]] = 0
-        X = relu(X)
-    return X
-
-def normalize_cols(X, eps=0.001):
-    X = X / (np.linalg.norm(X, axis=0) + eps)
-    return X
-
 class Connectivity:
     
     def __init__(self, X, k):
         """
         Parameters
         ----------
-        X : NumPy array of size N x m x k where N is the number of subjects, 
+        X : NumPy array of size N x m x p where N is the number of subjects, 
             m is the number of observations and p the dim of observartion space
         k : int
             Nb of latent variables
@@ -33,7 +16,7 @@ class Connectivity:
         
         self.k = k 
         
-        # self.v = np.ones(self.N) # Not included in this version
+        self.v = np.ones(self.N) 
                 
         self.G = np.concatenate([np.eye(k)[np.newaxis, ...]\
                                  for i in range(self.N)], axis=0)
@@ -50,7 +33,6 @@ class Connectivity:
         self.lagrange = np.zeros((self.k,self.k))
         
     def initialize_W(self):
-        
         X_cov = np.zeros((self.p, self.p))
         
         for i in range(self.N):
@@ -60,23 +42,20 @@ class Connectivity:
         # initialize W
         evd_X_cov = np.linalg.eig(X_cov)
         
-        self.W = evd_X_cov[1][:, evd_X_cov[0].argsort()[::-1][:self.k]]
+        self.W = evd_X_cov[1][:, evd_X_cov[0].argsort()[::-1][:k]]
         
         # since evectors are sign invariant
         for i in range(self.W.shape[1]):
             if np.sum(self.W[:,i]) < 0:
-                
                 self.W[:,i] *= -1
                 
         self.W = relu(self.W)
 
     
     def negative_log_likelihood(self):
-
         log_likelihood = 0
         
         for i in range(self.N):
-            
             cst = self.p * np.log(2*np.pi)
             
             det_sigma = np.log(np.linalg.det(self.sigmas[i]))
@@ -89,13 +68,24 @@ class Connectivity:
         
         return log_likelihood.sum()/(self.n * self.N)
     
-    
+    def update_v(self, lr):
+        for i in range(self.N):
+            d, V = np.linalg.eig(self.G[i])
+            D_tilde = V @ np.diag(-d/(d+self.v[i])**2) @ V.T
+            D_bar = V.T @ np.diag(-2*d**2/(d+self.v[i])**3) @ V.T
+            H_1 = 2*self.A[i] - self.v[i]*D_tilde - self.A[i] @ self.A[i] + self.v[i] * D_bar
+            H_2 = -self.v[i]**-2 * np.trace(self.A[i] - self.v[i]*D_tilde)  
+            v_term_1 = self.v[i]**-3 * np.trace(self.v[i]* np.eye(self.p) - self.K[i])
+            v_term_2 = self.v[i]**-3 * np.trace(self.W.T @ self.K[i] @ self.W @ H_1)
+            
+            self.v[i] -= lr * (v_term_1 + v_term_2 + H_2)
+        
     def update_A(self):
         
         W = project_positive(self.W).copy()
         
         for i in range(self.N):
-            A_new = np.eye(self.k) - np.linalg.pinv(W.T @ self.K[i] @ W)
+            A_new = np.eye(self.k) - self.v[i]*np.linalg.pinv(W.T @ self.K[i] @ W)
         
             # check for negative eigenvalues
             if np.min(np.linalg.eig(A_new)[0]) <= 0:
@@ -110,7 +100,6 @@ class Connectivity:
         iter = 0
         
         while stopBackTracking==False:
-            
             W_new = self.W - alpha * W_grad
             
             W_new = relu(W_new)
@@ -120,12 +109,10 @@ class Connectivity:
             new_obj  = 0
             
             for i in range(self.N):
-                
                 old_obj += np.diag(self.W.T @ self.K[i] @ self.W @ A_tilde[i]).sum()
                 new_obj += np.diag(W_new.T @ self.K[i] @ W_new @ A_tilde[i]).sum()
                 
             if new_obj <= old_obj + c*alpha*(np.diag(np.diag(W_grad.T @ (W_new - self.W))).sum() + 0.001):
-                
                 stopBackTracking = True
                 
             else:
@@ -134,33 +121,29 @@ class Connectivity:
                 iter += 1
                 
                 if iter > max_iter:
-                    
                     stopBackTracking = True
                 
         return W_new
     
     def fit(self, lr = 0.001, reg = 1, c = 0.01, alpha = 0.5, tol=1e-2, max_iter = 1000, use_armijo=True, estimate_v=False):
         
+        # Inits
         new_likelihood = float('-inf')
         old_likelihood = 0
         log_likelihoods = []
         W_old = np.copy(self.W)
         
-        self.update_A()
         
+        self.update_A() # Update A once before the loop
         for iter in range(max_iter):
 
-#            print(f"iteration {iter}")
-#            print("LL =",new_likelihood)
-            
             A_tilde = np.array([0.5*A_i @ A_i - A_i for A_i in self.A])
             
             # Compute W_grad
             W_grad = np.zeros(self.W.shape)
             
             for i in range(self.N):
-                W_grad = W_grad + 1/self.N * self.K[i] @ self.W @ A_tilde[i]
-                #TODO: check casting error when using "+=", error: Cannot cast ufunc 'add' output from dtype('complex128') to dtype('float64') with casting rule 'same_kind'
+                W_grad += self.v[i] * self.K[i] @ self.W @ A_tilde[i] / self.N
             
             penalty = reg * (self.W @ self.W.T @ self.W - self.W)                                 
             ortho = self.W @ self.lagrange
@@ -172,14 +155,19 @@ class Connectivity:
             else:
                 self.W -= lr * W_grad
             
+            # Make sure W respects identifiability constraints
             self.W = project_positive(self.W)
             self.W = relu(self.W) # to ensure non-negativity and orthonormality 
             self.W = normalize_cols(self.W)
             
+            # Update A
             self.update_A()
+
+            # Update v if necessary
+            if estimate_v:
+                self.update_v(lr)
             
-            self.lagrange = self.lagrange + reg * (self.W.T @ self.W - np.eye(self.k))
-            #TODO: check casting error when using "+=", error: Cannot cast ufunc 'add' output from dtype('complex128') to dtype('float64') with casting rule 'same_kind'
+            self.lagrange += reg * (self.W.T @ self.W - np.eye(self.k))
             
             # update G and sigmas
             self.G = np.array([self.W.T @ self.K[i] @ self.W - np.eye(self.k) for i in range(self.N)])
@@ -187,14 +175,13 @@ class Connectivity:
             self.sigmas = np.concatenate([(self.W @ self.G[i] @ self.W.T\
                                        + np.eye(self.p))[np.newaxis, ...] for i in range(self.N)])
             
-            # -------- check for convergence --------
-            
             if np.sum(np.abs(self.W - W_old)) < tol:
-            # if np.sum(np.abs(new_likelihood - old_likelihood)) < tol:
                 break
             
             else:
                 W_old = np.copy(self.W)
+
+            # Log information    
             old_likelihood = new_likelihood
             new_likelihood = self.negative_log_likelihood()
             log_likelihoods.append(new_likelihood)
